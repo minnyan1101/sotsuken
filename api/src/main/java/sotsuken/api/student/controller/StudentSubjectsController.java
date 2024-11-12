e8509d1688e31b69c9863e0643f0455825d1dcd8
enokonogusa <numakuro77@gmail.com>
2024-11-12 04:04:01 +0000

    api: StudentSubjectsController.javaの利用していないimportの削除

--- api/src/main/java/sotsuken/api/student/controller/StudentSubjectsController.java
+++ api/src/main/java/sotsuken/api/student/controller/StudentSubjectsController.java
@@ -7,8 +7,6 @@ import java.time.LocalDate;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @RestController
