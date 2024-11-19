ec22850f6ab4a8187763759187afaaa709cd51d7
minnyan1101 <kcsoita.20210178@gmail.com>
2024-11-19 00:46:46 +0000

    CreateClassesUseCaseの作成 Fixes #43

--- src/main/java/sotsuken/api/teacher/controller/ClassesController.java
+++ src/main/java/sotsuken/api/teacher/controller/ClassesController.java
@@ -4,9 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import sotsuken.api.teacher.service.CreateClassesUseCase;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 @RequestMapping("/api/teacher/classes")
 public class ClassesController {
+
+    @Autowired
+    CreateClassesUseCase createClassesUseCase;
 
     @GetMapping("")//クラス一覧表示
     public List<ClassResponse> fetchAllClass() {
@@ -25,11 +32,8 @@ public class ClassesController {
     }
     
     @PostMapping("")
-    public ClassResponse createClass(@RequestBody CreateClassRequest createclass) {
-        return new ClassResponse(
-        0L,    
-        "xxx"
-        );
+    public ClassResponse createClass(@AuthenticationPrincipal UserDetails userDetails, @RequestBody CreateClassRequest createclass) {
+        return createClassesUseCase.handle(userDetails.getUsername(), createclass.className);
     }
 
     @PostMapping("/{classId}")
