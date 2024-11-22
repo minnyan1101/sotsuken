d49e5b9a9903f1d97ae1b59432a23ac840027ec0
MINEMATU <kcsoita20210256@gmail.com>
2024-11-22 00:34:45 +0000

    StudentClassモデルの制約の作成 Fixes #92

--- src/main/java/sotsuken/api/model/StudentClass.java
+++ src/main/java/sotsuken/api/model/StudentClass.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import sotsuken.api.model.exception.ClassNameValidationException;
 
 @Entity
 @Table(name = "classes")
@@ -18,8 +19,21 @@ public class StudentClass {
     }
 
     public StudentClass(Long id, String name) {
+        classNameValidation(name);
         this.id = id;
         this.name = name;
+    }
+
+    private static void classNameValidation(String name) {
+        //  nameは1~16文字
+        if ( ! (1 <= name.length() && name.length() <= 16)) {
+            throw new ClassNameValidationException();
+        }
+
+        // nameは一行からなる
+        if ( ! (name.matches(".*\\v.*"))) { // 改行に類する空白文字が文字列に含まれているか確認している
+            throw new ClassNameValidationException();
+        }
     }
 
     public Long getId() {
@@ -31,6 +45,7 @@ public class StudentClass {
     }
 
     public void changeName(String newName) {
+        classNameValidation(newName);
         this.name = newName;
     }
 }
