c44e971be10c44d00a45d0a2c9db6ebb5488f13d
minnyan1101 <kcsoita.20210178@gmail.com>
2024-11-22 02:02:56 +0000

    StudentAccountControllerの作成 Fixes #88

---
+++ src/main/java/sotsuken/api/student/controller/StudentAccountController.java
@@ -0,0 +1,45 @@
+package sotsuken.api.student.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sotsuken.api.student.service.RegisterStudentAccountUseCase;
+import sotsuken.api.teacher.controller.StudentClassResponse;
+import sotsuken.api.teacher.service.FetchAllClassesUseCase;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentAccountController {
+
+    @Autowired
+    FetchAllClassesUseCase fetchAllClassesUseCase;
+
+    @Autowired
+    RegisterStudentAccountUseCase registerStudentAccountUseCase;
+
+    @PostMapping("register")
+    public StudentAccountResponse registerStudentAccount(@RequestBody RegisterStudentAccountRequest request) {
+        return registerStudentAccountUseCase.handle(request.studentId, request.studentName, request.password, request.classId);
+    }
+
+    @GetMapping("classes")
+    public List<StudentClassResponse> fetchAllStudentClasses(
+        @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        return fetchAllClassesUseCase.handle(userDetails.getUsername());
+    }
+    
+    
+}
---
+++ src/main/java/sotsuken/api/student/controller/StudentAccountResponse.java
@@ -0,0 +1,11 @@
+package sotsuken.api.student.controller;
+
+public class StudentAccountResponse {
+    
+    public String studentId;
+
+    public StudentAccountResponse(String studentId) {
+        this.studentId = studentId;
+    }
+
+}
