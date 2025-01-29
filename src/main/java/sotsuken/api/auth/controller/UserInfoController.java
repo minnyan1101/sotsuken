41caf9c5ba35c32b1f9108478648d6c95aed862c
minnyan1101 <kcsoita.20210178@gmail.com>
2025-01-29 01:32:48 +0000

    AuthAPIのUserInfoにユーザー名を追加する Fixes #252

--- src/main/java/sotsuken/api/auth/controller/UserInfoController.java
+++ src/main/java/sotsuken/api/auth/controller/UserInfoController.java
@@ -8,6 +8,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import sotsuken.api.model.Account;
 import sotsuken.api.model.AccountType;
 import sotsuken.api.repository.AccountRepository;
+import sotsuken.api.repository.StudentRepository;
+import sotsuken.api.repository.TeacherRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,6 +26,12 @@ public class UserInfoController {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    TeacherRepository teacherRepository;
+
     @GetMapping("/userinfo")
     @Operation(summary = "認証したアカウントの名前と権限を取得")
     public UserInfoResponse fetchUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
@@ -31,13 +39,34 @@ public class UserInfoController {
         Account account = accountRepository.findById(userDetails.getUsername())
                 .orElseThrow(() -> new ErrorResponseException(HttpStatus.NOT_FOUND));
 
+        String userName;
+
+        switch (account.getAccountType()) {
+            case ADMIN:
+                userName = "ADMIN";
+                break;
+        
+            case TEACHER:
+                userName = teacherRepository.findById(account.getUserName()).get().getName();
+                break;
+
+            case STUDENT:
+                userName = studentRepository.findById(account.getUserName()).get().getName();
+                break;
+
+            default:
+                throw new ErrorResponseException(HttpStatus.NOT_FOUND);
+        }
+
         return new UserInfoResponse(
                 userDetails.getUsername(),
+                userName,
                 account.getAccountType());
     }
 
     public record UserInfoResponse(
             String id,
+            String name,
             AccountType role) {
     }
 }
